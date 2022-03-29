b8d6ce76ec9933c250939130c960019fa69a2748
Prekzohaw <worikoff@ya.ru>
2022-03-29 18:45:13 +0300

    new level and hazard

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    float movementFactor;
+    [SerializeField] float period = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float cycles = Time.time / period; //grows over time
+
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(tau*cycles);
+
+        movementFactor = Mathf.Abs(rawSinWave);
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+}
